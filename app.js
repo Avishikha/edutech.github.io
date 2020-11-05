7781daf5e69a9478da665c96a53d5cc35a27ef8c
Avishikha <59833686+Avishikha@users.noreply.github.com>
2020-11-05 14:18:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
